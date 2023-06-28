24a9ccbfa050bee3c23c9dcb66412ac791ec786b
Dayana-Cris <68671879+Dayana-Cris@users.noreply.github.com>
2023-06-27 18:47:42 -0600

    Cambios
    Cambios Index

--- 2023 UCR/mvc_proyecto/view/main/index.php
+++ 2023 UCR/mvc_proyecto/view/main/index.php
@@ -3,21 +3,60 @@ require 'view/header.php';
 require 'view/menu.php';
 ?>
 
-<body class="colorFondo">
+<body>
     <div class="container-fluid imgFondoMain" id="contendorprincipal ">
-
-        <div class="container">
+        <div class="container-fluid m-0 p-0">
+        <div id="carouselId" class="carousel slide" data-bs-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-bs-target="#carouselId" data-bs-slide-to="0" class="active" aria-current="true"
+                        aria-label="First slide"></li>
+                    <li data-bs-target="#carouselId" data-bs-slide-to="1" aria-label="Second slide"></li>
+                    <li data-bs-target="#carouselId" data-bs-slide-to="2" aria-label="Third slide"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                        <img src="public/img/Inicio/Carrusel1.jpeg" class="w-100 d-block" alt="First slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h1 class="fw-bold">Universidad DC</h1>
+                            <p>Ofrecemos distintas carreras que se ajustan a tus sueños.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="public/img/Inicio/Carrusel2.jpeg" class="w-100 d-block" alt="Second slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h3>Profesores</h3>
+                            <p>Contamos con más de 100 profesores capacitados para enseñarte de una manera innovadora.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="public/img/Inicio/Carrusel3.jpeg" class="w-100 d-block" alt="Third slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h3>Estudiantes</h3>
+                            <p>Nuestros estudiantes, cuentan con espacios abiertos donde pueden relajarse y estudiar
+                                tranquilamente</p>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
             <div class="container-fluid">
                 <div class="text-center mt-3">
                     <hr class="text-light">
-                    <h1>CYBER DC</h1>
-                    <h2 class="text-center mt-3">INSTITUTO EDUCATIVO</h2>
+                    <h2 class="text-center mt-3">INSTITUCIÓN EDUCATIVA</h2>
                     <hr class="text-light">
                 </div>
-                <div id="" class="py-5">
+                <div id="" class="py-3">
                     <div class="container">
                         <div class="row">
-                            <div class="col-md-4 mb-4">
+                            <div class="col-md-4 mb-4 ">
                                 <div class="card h-100">
                                     <img src="public/img/Inicio/Biblioteca.jpg" class="card-img-top" alt="Imagen de la biblioteca">
                                     <div class="card-body">
@@ -56,36 +95,6 @@ require 'view/menu.php';
                                             habilidades y
                                             conocimientos relacionados con la informática. Con equipos modernos y actualizados, </p>
                                     </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div class="row mb-4">
-                        <div class="col-lg-6 mt-2">
-                            <div class="card h-100">
-                                <img src="public/img/Inicio/profesor.jpg" class="card-img-top" alt="Imagen del servicio">
-                                <div class="card-body">
-                                    <p class="card-text text-center">En nuestro institutos, contamos con un excelente grupo de profesores,
-                                        acompañamos
-                                        a nuestros estudiantes por una buena educación, de calidad, puedes gestionar la información de los
-                                        profesores, los cursos, grupos y
-                                        estudiantes del instituto.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 mt-2">
-                            <div class="card h-100">
-                                <img src="public/img/Inicio/estudiando.jpg" class="card-img-top" alt="Imagen del servicio">
-                                <div class="card-body">
-                                    <p class="card-text text-center">En nuestro institutos, contamos con un excelente grupo de profesores,
-                                        acompañamos
-                                        a nuestros estudiantes por una buena educación, de calidad, puedes gestionar la información de los
-                                        profesores, los cursos, grupos y
-                                        estudiantes del instituto.
-                                    </p>
                                 </div>
                             </div>
                         </div>
